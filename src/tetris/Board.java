bd18b2ffb3bc2e5e21469c59ef1e2dbd37efeadb
Viktoriya Khegay <hosunny0909@gmail.com>
2025-11-01 01:59:19 +0900

    Add basic Collision Detection and shape falling

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -18,15 +18,59 @@ public class Board extends JPanel {
             {null, Color.RED, null},
     };
 
+    public boolean canMove(int deltaX, int deltaY) {
+        for (int row = 0; row < shape.length; row++) {
+            for (int col = 0; col < shape[row].length; col++) {
+                if (shape[row][col] != null) {
+                    int newX = col + deltaX;
+                    int newY = row + deltaY;
+
+                    if (newX < 0 || newX >= BOARD_WIDTH || newY < 0 || newY >= BOARD_HEIGHT) {
+                        return false;
+                    }
+
+                    if (board[newX][newY] != null) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
     public Board() {
         looper = new Timer(1000, new ActionListener() {
-            int n = 0;
             @Override
-            public void  actionPerformed(ActionEvent e){
-                System.out.println(n++);
+            public void actionPerformed(ActionEvent e) {
+                if (canMove(0, 1)) {
+                    moveShapeDown();
+                } else {
+                    fixShape();
+                }
+                repaint();
             }
         });
         looper.start();
+    }
+
+    private void moveShapeDown() {
+        Color[][] newShape = new Color[shape.length][shape[0].length];
+        for (int r = 0; r < shape.length; r++) {
+            for (int c = 0; c < shape[r].length; c++) {
+                newShape[r][c] = shape[r][c];
+            }
+        }
+        shape = newShape;
+    }
+
+    private void fixShape() {
+        for (int r = 0; r < shape.length; r++) {
+            for (int c = 0; c < shape[r].length; c++) {
+                if (shape[r][c] != null) {
+                    board[c][r] = shape[r][c];
+                }
+            }
+        }
     }
 
     @Override
@@ -35,24 +79,30 @@ public class Board extends JPanel {
         g.setColor(Color.gray);
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        //draw the shape
         for (int row = 0; row < shape.length; row++) {
             for (int col = 0; col < shape[row].length; col++) {
                 if (shape[row][col] != null) {
                     g.setColor(shape[row][col]);
-                g.fillRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE );
+                    g.fillRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
                 }
             }
         }
 
-        //draw the board
         g.setColor(Color.white);
         for (int row = 0; row < BOARD_HEIGHT; row++) {
-            g.drawLine(0, BLOCK_SIZE * row,BLOCK_SIZE * BOARD_WIDTH, BLOCK_SIZE * row);
+            g.drawLine(0, BLOCK_SIZE * row, BLOCK_SIZE * BOARD_WIDTH, BLOCK_SIZE * row);
         }
-
         for (int col = 0; col < BOARD_WIDTH; col++) {
             g.drawLine(col * BLOCK_SIZE, 0, col * BLOCK_SIZE, BLOCK_SIZE * BOARD_HEIGHT);
+        }
+
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            for (int col = 0; col < BOARD_WIDTH; col++) {
+                if (board[col][row] != null) {
+                    g.setColor(board[col][row]);
+                    g.fillRect(col * BLOCK_SIZE, row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                }
+            }
         }
     }
 }
