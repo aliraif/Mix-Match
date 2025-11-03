ee42ecae707d2a6e735f81ee493597c8c924bdc6
naufal <naufalismail@hanyang.ac.kr>
2025-11-03 19:53:44 +0900

    feature/boundary check and stopping

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -31,13 +30,30 @@ public class Board extends JPanel implements KeyListener {
     private int delayTimeForMovement = normal;
     private long beginTime;
 
+    private int deltaX = 0;
+    private boolean collision = false;
+
     public Board() {
         looper = new Timer(delay, new ActionListener() {
             int n = 0;
             @Override
             public void  actionPerformed(ActionEvent e){
+                if(collision) {
+                    return;
+                }
+
+                // check moving horizontal
+                if(!(x + deltaX + shape[0].length > 10) && !(x + deltaX < 0)) {
+                    x+= deltaX;
+                }
+                deltaX = 0;
+
                 if(System.currentTimeMillis() - beginTime > delayTimeForMovement) {
-                    y++;
+                    if(!(y + 1 + shape.length > BOARD_HEIGHT)) {
+                        y++;
+                    } else {
+                        collision = true;
+                    }
                     beginTime = System.currentTimeMillis();
                 } 
                 repaint();
@@ -80,6 +96,10 @@ public class Board extends JPanel implements KeyListener {
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             delayTimeForMovement = fast;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            deltaX = 1;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            deltaX = -1;
         }
     }
 
